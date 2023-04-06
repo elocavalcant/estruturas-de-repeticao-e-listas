4d0d035021788a5dde381afda622821a6a408024
unknown <eloahcavalcantesenai@gmail.com>
2023-04-06 15:25:57 -0300

    Neste arquivo foi feito um programa que leu um vetor de 10 caracteres, onde o mesmo analisou e nos deu o valor das consoantes

---
+++ exercicio4.py
@@ -0,0 +1,10 @@
+caracteres= ['a', 'c', 'e', 'i', 'p', 'j', 'l','q', 't', 'o']
+consoantes= ['b','c','j','l','p','q','t']
+for i in caracteres:
+    if i not in "a,e,i,o,u": 
+        print(i)
+x=len(consoantes)
+print("Total de consoantes:" ,x)
+
+
+
