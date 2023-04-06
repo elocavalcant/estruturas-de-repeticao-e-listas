61ca912881c3bf78cb7bd012eee41f1034e99300
unknown <eloahcavalcantesenai@gmail.com>
2023-04-06 11:14:26 -0300

    Neste arquivo & C:/Users/05/AppData/Local/Programs/Python/Python311/python.exe c:/dev/estruturas-de-repeticao-e-listas/exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,6 +1,12 @@
-realnumlist=  [1, 2, 3,4,5, 6, 7, 8, 9,10]
+realnumlist=  [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+
+
+variavel=0
+
+for i in range (10):
+    variavel=realnumlist.pop()
+    print (variavel)
+
 print(realnumlist)
-
-
 
 
