e6ba6245d44841112f26726a225bdebc413ed313
unknown <eloahcavalcantesenai@gmail.com>
2023-04-14 14:56:28 -0300

    Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.

---
+++ exerciciorepeticao10.py
@@ -0,0 +1,7 @@
+numero1=int(input("digite um numero: "))
+numero2=int(input("digite outro numero: "))
+
+for n in range(numero1+1,numero2):
+    print (n)
+for n in range(numero2+1, numero1):
+    print(n)
